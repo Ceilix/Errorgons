1882c5335517194493b42a3dd5fa18214324730d
Ceilix <51411640+Ceilix@users.noreply.github.com>
2019-06-17 11:37:29 +0800

    Add files via upload
    Added login function & Bugfixes

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import jinja2
 import json
 import HappyCloud
 from google.appengine.api import urlfetch
-
+from google.appengine.api import users
 
 jinja_current_dir = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
@@ -13,6 +13,17 @@ jinja_current_dir = jinja2.Environment(
 
 
 class MainHandler(webapp2.RequestHandler):
+      def get(self):
+          user = users.get_current_user()
+          if user:
+
+            # self.response.write("To be or not to be")
+            self.redirect("/HappyInterface")
+          else:
+        #     self.response.write("You are not logged in")
+            self.redirect("/nouser")
+
+class MainPage(webapp2.RequestHandler):
       def get(self):
          print('Mainhandlerhello')
          start_template = jinja_current_dir.get_template("templates/Interface.html")
@@ -23,7 +34,6 @@ class MainHandler(webapp2.RequestHandler):
 class HappyInput(webapp2.RequestHandler):
       def post(self):
           start_template = jinja_current_dir.get_template("templates/input_page.html")
-
           Happythought1 = self.request.get('happy_input1')
           Happythought2 = self.request.get('happy_input2')
           Happythought3 = self.request.get('happy_input3')
@@ -32,10 +42,10 @@ class HappyInput(webapp2.RequestHandler):
               input2 =  Happythought2,
               input3 =  Happythought3
           )
-
+          
           Happy.put()
 
-          self.response.write(start_template.render(Happy))
+          self.response.write(start_template.render())
 
 class HappyRetreive(webapp2.RequestHandler):
       def post(self):
@@ -53,9 +63,16 @@ class HappyLibrary(webapp2.RequestHandler):
     def get(self):
         results_template = the_jinja_env.get_template('templates/library.html')
 
+class NoUserHandler(webapp2.RequestHandler):
+      def get(self):
+          login_url = users.create_login_url("/")
+          self.response.write("Please log in. <a href="+ login_url + ">Login There</a>")
+
 app = webapp2.WSGIApplication([
      ('/',MainHandler),
+     ('/HappyInterface',MainPage),
      ('/Happy', HappyInput),
      ('/Happier',HappyRetreive),
-     ('/library', HappyLibrary)
+     ('/library', HappyLibrary),
+     ('/nouser', NoUserHandler)
 ], debug=True)
