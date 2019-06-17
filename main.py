4e32669bf234d29fe21c3588982087908a005b8f
demo <demo@provisions-MacBook-Air-2.local>
2019-06-17 15:20:13 +0800

    made database functionable

--- main.py
+++ main.py
@@ -34,21 +34,22 @@ class MainPage(webapp2.RequestHandler):
 class HappyInput(webapp2.RequestHandler):
       def post(self):
           start_template = jinja_current_dir.get_template("templates/input_page.html")
-          Happythought1 = self.request.get('happy_input1')
-          Happythought2 = self.request.get('happy_input2')
-          Happythought3 = self.request.get('happy_input3')
-          Happy = HappyCloud.Happy (
-              input1 =  Happythought1,
-              input2 =  Happythought2,
-              input3 =  Happythought3
-          )
-
-          Happy.put()
-
           self.response.write(start_template.render())
+
+
 
 class HappyRetreive(webapp2.RequestHandler):
       def post(self):
+          Happythought1 = self.request.get('Happy_thought1')
+          Happythought2 = self.request.get('Happy_thought2')
+          Happythought3 = self.request.get('Happy_thought3')
+          Happy = HappyCloud.Happy (
+               input1 =  Happythought1,
+               input2 =  Happythought2,
+               input3 =  Happythought3
+          )
+
+          Happy.put()
           start_template = jinja_current_dir.get_template("templates/Readthoughts.html")
           happy_query = HappyCloud.Happy.query()
           happy = happy_query.fetch()
@@ -56,18 +57,19 @@ class HappyRetreive(webapp2.RequestHandler):
           template_vars = {
                 'happy' : happy,
           }
-
           self.response.write(start_template.render(template_vars))
 
 class HappyLibrary(webapp2.RequestHandler):
-    def post(self):
-        start_template = jinja_current_dir.get_template('templates/library.html')
+    def get(self):
+        results_template = jinja_current_dir.get_template('templates/library.html')
+
         happy_query = HappyCloud.Happy.query()
         happy = happy_query.fetch()
 
         template_vars = {
-               'happy' : happy,
+               'happy' : happy
                }
+        self.response.write(results_template.render(template_vars))
 
 class NoUserHandler(webapp2.RequestHandler):
       def get(self):
