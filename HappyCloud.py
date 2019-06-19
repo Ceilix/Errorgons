ee777ec225c627a67a11e6c6d0dd3524b49eb32f
Ceilix <51411640+Ceilix@users.noreply.github.com>
2019-06-19 12:25:01 +0800

    Updated P0 Files :)

--- HappyCloud.py
+++ HappyCloud.py
@@ -1,8 +1,44 @@
 from google.appengine.ext import ndb
 
-class Happy(ndb.Model):
-    input1 = ndb.StringProperty(required = True)
-    input2 = ndb.StringProperty(required = True)
-    input3 = ndb.StringProperty(required = True)
-    # HappyMessage = ndb.StringProperty(required = True)
-    User = ndb.StringProperty(required = False)
+class ModelUser(ndb.Model):
+    email = ndb.StringProperty(required = True)
+    input = ndb.StringProperty(required = True)
+    def __init__(self,counter = 0):
+
+     user_id = create_account()
+     return user_id
+
+    def Happyupdate(self):
+     counter.self = ndb.StringProperty(required = True)
+     Happy_count = counter.self
+
+    def __init__(self,counter = 0):
+
+     user_id = create_account()
+     return user_id
+
+    def create_account(self, new_account):
+
+        #Maybe create function
+        if new_account.username not in self.users:
+           self.users[new_account.username] = new_account
+
+           #create
+        else:
+            return False
+
+
+    # def get_by_user(cls, user):
+    #     return cls.query().filter(cls.user_id) == user.user_id().get()
+
+
+
+
+
+
+# class Happy(ndb.Model):
+#     input1 = ndb.StringProperty(required = True)
+#     input2 = ndb.StringProperty(required = False)
+#     input3 = ndb.StringProperty(required = False)
+#     # HappyMessage = ndb.StringProperty(required = True)
+#     User = ndb.StringProperty(required = False)
--- main.py
+++ main.py
@@ -3,18 +3,56 @@ import os
 import jinja2
 import json
 import HappyCloud
+from HappyCloud import ModelUser
+import random
+import shelve
 from google.appengine.api import urlfetch
 from google.appengine.api import users
+
 
 jinja_current_dir = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
 
+def get_message():
+    message_list=['Tomorrow, you will meet a life-changing new friend.',
+                  'Fame and Instagram followers are headed your way.',
+                  'On the Tuesday after next, an odd meeting will lead to a new opportunity.',
+                  'Despite dry skies, bring an umbrella tomorrow.',
+                  'A thrilling time is in your immediate future.',
+                  'Someone has Googled you recently.',
+                  'Stay alert. You will be part of a rescue mission.',
+                  'You will beat Watson in a game of Jeopardy. Start studying though']
+    return(random.choice(message_list))
+
+def get_goals():
+    message_list=['Tomorrow, you will meet a life-changing new friend.',
+                  'Fame and Instagram followers are headed your way.',
+                  'On the Tuesday after next, an odd meeting will lead to a new opportunity.',
+                  'Despite dry skies, bring an umbrella tomorrow.',
+                  'A thrilling time is in your immediate future.',
+                  'Someone has Googled you recently.',
+                  'Stay alert. You will be part of a rescue mission.',
+                  'You will beat Watson in a game of Jeopardy. Start studying though']
+    return(random.choice(message_list))
+
+
+
+
+def Happy_goals2():
+    ModelUser.Happyupdate()
+    return Happy_count
+    #Create object for user
+    #Append Happy count when submit is clicked
+    #Return no. of items in Happy count to get a Daily_Happiness_Counter
+    #Clear Happy_Count when HappyLibrary is accessed!
+
 
 class MainHandler(webapp2.RequestHandler):
       def get(self):
           user = users.get_current_user()
+
           if user:
 
             # self.response.write("To be or not to be")
@@ -27,29 +65,43 @@ class MainPage(webapp2.RequestHandler):
       def get(self):
          print('Mainhandlerhello')
          start_template = jinja_current_dir.get_template("templates/Interface.html")
-         Icons = {
-          }
-         self.response.write(start_template.render(Icons))
+         # Userinfo = Dict(
+         #          email = users.get_current_user().nickname())
+
+
+
+
+
+         self.response.write(start_template.render())
 
 class HappyInput(webapp2.RequestHandler):
       def post(self):
+          Happy_goals2()
           start_template = jinja_current_dir.get_template("templates/input_page.html")
-          self.response.write(start_template.render())
+          Happythought1 = self.request.get('happy_input1')
+          # Happythought2 = self.request.get('happy_input2')
+          # Happythought3 = self.request.get('happy_input3')
+          Happy = HappyCloud.Happyupdate(
+              input1 =  Happythought1,
+          )
+          #How do I run a function from Happycloud on main.py()
+          #How do I put this in the function
+          Happy_counter = Happy_count
+          Happymessage = get_message()
+          Goals = get_goals()
 
 
+
+          Happyvar = {
+             'Daily_Happiness_Counter' : Happy_counter,
+             'Wellness_Messages' : Happymessage,
+             'Happy_goals' : Goals
+          }
+
+          self.response.write(start_template.render(Happyvar))
 
 class HappyRetreive(webapp2.RequestHandler):
       def post(self):
-          Happythought1 = self.request.get('Happy_thought1')
-          Happythought2 = self.request.get('Happy_thought2')
-          Happythought3 = self.request.get('Happy_thought3')
-          Happy = HappyCloud.Happy (
-               input1 =  Happythought1,
-               input2 =  Happythought2,
-               input3 =  Happythought3
-          )
-
-          Happy.put()
           start_template = jinja_current_dir.get_template("templates/Readthoughts.html")
           happy_query = HappyCloud.Happy.query()
           happy = happy_query.fetch()
@@ -57,24 +109,30 @@ class HappyRetreive(webapp2.RequestHandler):
           template_vars = {
                 'happy' : happy,
           }
+
           self.response.write(start_template.render(template_vars))
 
 class HappyLibrary(webapp2.RequestHandler):
-    def get(self):
-        results_template = jinja_current_dir.get_template('templates/library.html')
-
+    def post(self):
+        start_template = jinja_current_dir.get_template('templates/library.html')
         happy_query = HappyCloud.Happy.query()
         happy = happy_query.fetch()
 
         template_vars = {
-               'happy' : happy
+               'happy' : happy,
                }
-        self.response.write(results_template.render(template_vars))
 
 class NoUserHandler(webapp2.RequestHandler):
       def get(self):
           login_url = users.create_login_url("/")
           self.response.write("Please log in. <a href="+ login_url + ">Login There</a>")
+
+class NoUserHandler(webapp2.RequestHandler):
+      def get(self):
+          login_url = users.create_login_url("/")
+          self.response.write("Please log in. <a href="+ login_url + ">Login There</a>")
+
+
 
 app = webapp2.WSGIApplication([
      ('/',MainHandler),
@@ -84,4 +142,3 @@ app = webapp2.WSGIApplication([
      ('/library', HappyLibrary),
      ('/nouser', NoUserHandler)
 ], debug=True)
-# random.choice() use this to do the randomizer tomorrow
