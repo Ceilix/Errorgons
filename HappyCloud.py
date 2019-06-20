334748f4ea8c4011e7e0b17b7bec945790363505
Ceilix <51411640+Ceilix@users.noreply.github.com>
2019-06-20 15:31:03 +0800

    Updated python files w Survey classifications

--- HappyCloud.py
+++ HappyCloud.py
@@ -4,6 +4,8 @@ class Happy(ndb.Model):
     input1 = ndb.StringProperty(required = False)
     # HappyMessage = ndb.StringProperty(required = True)
     User = ndb.StringProperty(required = True)
+    UserAge = ndb.StringProperty(required = False)
+    UserRegion = ndb.StringProperty(required = False)
 
 class Usertemp(ndb.Model):
     Userkey = ndb.StringProperty(required=True)
