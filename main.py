c2b9dce048ad8ef29b4e16b87edc5488d8658721
Ceilix <51411640+Ceilix@users.noreply.github.com>
2019-06-20 13:29:53 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import jinja2
 import json
 import random
 import HappyCloud
+from HappyCloud import Happy
 from HappyCloud import Usertemp
 from google.appengine.api import urlfetch
 from google.appengine.api import users
@@ -53,9 +54,11 @@ class MainHandler(webapp2.RequestHandler):
             )
 
             Userinit.put()
+            # self.response.write("To be or not to be")
             self.redirect("/HappyInterface")
 
           else:
+        #     self.response.write("You are not logged in")
             self.redirect("/nouser")
 
 class MainPage(webapp2.RequestHandler):
@@ -70,11 +73,11 @@ class MainPage(webapp2.RequestHandler):
 
 class HappySurvey(webapp2.RequestHandler):
     def get(self):
-        start_template = jinja_current_dir.get_template("templates/survey.html")
+        start_template = jinja_current_dir.get_template("templates/Survey.html")
         self.response.write(start_template.render())
     def post(self):
-        start_template = jinja_current_dir.get_template("templates/survey.html")
-        self.response.write(start_template.render())    
+        start_template = jinja_current_dir.get_template("templates/Survey.html")
+        self.response.write(start_template.render())
 
 
 class HappyInput(webapp2.RequestHandler):
@@ -111,16 +114,23 @@ class HappyInputClone(webapp2.RequestHandler):
          user = Usertemp.query(Usertemp.Userkey == str(users.get_current_user())).fetch()
          usercurrent = user[0]
          Happythought1 = self.request.get('Happy_thought1')
-         unlockbutton = ""
+         username = str(users.get_current_user())
          if Happythought1 != "":
-            usercurrent.Counter += 1
-            usercurrent.put()
+             Happy = HappyCloud.Happy(
+                    User = username,
+                   # Identity = surveyresults
+                    input1 =  Happythought1
+              )
+             usercurrent.Counter += 1
+             usercurrent.put()
+             Happy.put()
+
+         unlockbutton = ""
 
          if usercurrent.Counter >= 3:
             unlockbutton = "submit"
          else:
             unlockbutton = "hidden"
-
 
          #Get userlogin infoi
          #Use user login info id to get students
@@ -140,21 +150,23 @@ class HappyInputClone(webapp2.RequestHandler):
 
 class HappyRetreive(webapp2.RequestHandler):
       def post(self):
-          Happythought1 = self.request.get('Happy_thought1')
           user = Usertemp.query(Usertemp.Userkey == str(users.get_current_user())).fetch()
+          username = str(users.get_current_user())
+          Happythought1 = self.request.get('Happy_thought1')
+          username = str(users.get_current_user())
+          if Happythought1 != "":
+              Happythought1 = self.request.get('Happy_thought1')
+              Happy = HappyCloud.Happy(
+                     User = username,
+                    # Identity = surveyresults
+                     input1 =  Happythought1
+               )
+
+
+
           usercurrent = user[0]
           usercurrent.Counter = 0
           usercurrent.put()
-          username = str(users.get_current_user())
-          Happy = HappyCloud.Happy (
-               User = username,
-              # Identity = surveyresults
-               input1 =  Happythought1
-          )
-
-
-
-          Happy.put()
           start_template = jinja_current_dir.get_template("templates/Readthoughts.html")
           happy_query = HappyCloud.Happy.query()
           happy = happy_query.fetch()
@@ -168,18 +180,35 @@ class HappyLibrary(webapp2.RequestHandler):
     def get(self):
         results_template = jinja_current_dir.get_template('templates/library.html')
 
-        happy_query = HappyCloud.Happy.query()
-        happy = happy_query.fetch()
+        Userperm = Happy.query(Happy.User == str(users.get_current_user())).fetch()
+
+        def Number():
+            noomber = random.randint(0,len(Userperm)-1)
+            return noomber
+
+
+        Happythought1 = self.request.get('Happy_thought1')
+        unlockbutton = ""
+        done = True
+        Usernewer = Userperm[Number()]
+        new22 = str(Usernewer.input1)
+
+        #Ensure input is not = ""
+        #OR if input is "" refresh new22  until input retrieved not ""
+
+
+
 
         template_vars = {
-               'happy' : happy
+               'happy' : new22
                }
+
         self.response.write(results_template.render(template_vars))
 
 class NoUserHandler(webapp2.RequestHandler):
       def get(self):
           login_url = users.create_login_url("/")
-          self.response.write("Please log in. <a href="+ login_url + ">Login Here</a>")
+          self.response.write("Please log in. <a href="+ login_url + ">Login There</a>")
 
 app = webapp2.WSGIApplication([
      ('/',MainHandler),
@@ -191,4 +220,3 @@ app = webapp2.WSGIApplication([
      ('/library', HappyLibrary),
      ('/nouser', NoUserHandler)
 ], debug=True)
-# random.choice() use this to do the randomizer tomorrow
